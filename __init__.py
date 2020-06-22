add9a12ac22294eba10cf3b24792c99772429391
denis.tauchman@pslib.cz <denis.tauchman@pslib.cz>
2020-06-22 19:43:17 +0200

    added all users site

--- __init__.py
+++ __init__.py
@@ -77,7 +77,13 @@ def deleteAcc():
 	else:
 		return render_template('deleteAcc.html')
 
+@app.route('/allAccs/', methods=['POST', 'GET'])
+def adminDelete():
+	list = user.query.all()
+	return render_template('adminDel.html', users=list)
+
+
 
 if __name__ == '__main__':
 	db.create_all()
-	app.run(debug=True)
+	app.run(host='0.0.0.0',port=5000 ,debug=True)
